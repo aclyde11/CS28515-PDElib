35725ed5c49556d6c3799ca66acc497b708f13a0
aclyde11 <aclyde11@gmail.com>
2018-04-26 11:52:33 -0500

    Added arg parsing

--- gif_maker.py
+++ gif_maker.py
@@ -1,5 +1,5 @@
-import math
 import sys
+import os
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
@@ -26,6 +26,7 @@ dt = params[5]
 alpha = 1
 
 matrix = np.loadtxt(text_file + "tmp", usecols=range(int(nx + 1)))
+os.remove(text_file + "tmp")
 times = matrix[:, 0]
 matrix = np.delete(matrix, 0, 1)
 print times
@@ -60,13 +61,9 @@ def update(i):
     ax.plot(x, matrix[i, :], c="blue")
     return ax
 
-
 if __name__ == '__main__':
     # FuncAnimation will call the 'update' function for each frame; here
     # animating over 10 frames, with an interval of 200ms between frames.
     anim = FuncAnimation(fig, update, frames=np.arange(0, times.shape[0]), interval=1)
-    if len(sys.argv) > 1 and sys.argv[1] == 'save':
-        anim.save('line.gif', dpi=80, writer='imagemagick')
-    else:
-        # plt.show() will just loop the animation forever.
-        plt.show()
+    anim.save('line.gif', dpi=80, writer='imagemagick')
+    plt.show()
